e5f4fae56ebf602f4d1521348b9c107c480d7dab
Adrian Kirn <Adrian@adrian-kirns-macbook-2.local>
2016-09-25 17:15:53 -0700

    Adding the car-bot.ino arduino file and its containing folder.

---
+++ car-bot/car-bot.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(13,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+delay(1000);
+digitalWrite(13,!digitalRead(13));
+}
