6d4d8cacf4f05570dc70d82ffb5a5c3c5fa6ee41
Lawrence Hubbell <nathan@pellego.com>
2016-10-22 11:03:19 -0700

    got the sonar sensor working!!!!

--- sonar_test/sonar_test.ino
+++ sonar_test/sonar_test.ino
@@ -1,7 +1,10 @@
 unsigned long send_time = 0;
+
+unsigned long break_time = 0;
+
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
+Serial.begin(115200);
 
   pinMode(11, OUTPUT); 
   pinMode(10, INPUT); 
@@ -19,32 +22,34 @@ void loop() {
   
 //  if (millis()%50 == 0) {
 //    delay(1); // Must wait to avoid multiple pulses in one millis
-  delay(50);
+  delay(60);
+  break_time = millis(); 
   digitalWrite(9, LOW);
   delayMicroseconds(4);
   digitalWrite(9, HIGH);
   delayMicroseconds(10);
   digitalWrite(9, LOW);
-  send_time = millis();
-  Serial.println("Preparing pulse...");
-
   if (!digitalRead(10)) { // Right when the pules is tiggered, echo pin should be low
   
     while (!digitalRead(10))  { // Once the pulse is sent, the echo pin should go high, wait till then
-      if ((millis()-send_time) > 4000) break;
+        if ((millis()-break_time) > 4000) { // TIMEOUT
+          Serial.println("FUCK YOU FIRST!");
+          break;
+        }
     }
 
     if (digitalRead(10)) { // The pulse has been sent
-      Serial.println("    pulsed.");
-    
-      Serial.println("Waiting for echo...");
-      while (digitalRead(10))  {
-        if ((millis()-send_time) > 4000) break;
+      send_time = micros();
+      while (digitalRead(10))  { // Wait for the pulse to return
+        if ((millis()-break_time) > 4000) { // TIMEOUT
+          Serial.println("FUCK YOU!");
+          break;
+        }
       }
 
-      if (!digitalRead(10)) {
-        Serial.print("    Response time (millis): ");
-        Serial.println((millis()-send_time));
+      if (!digitalRead(10)) { // The prodigal pulse has returned!
+        Serial.print("    Distance (cm): ");
+        Serial.println((micros()-send_time)/58.0);
       }
     }
   }
